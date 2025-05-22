70dde06bea63d9b7a7b4361dfbac01f3f3e6f66d
USM <166993675+UltimateSpider-Man@users.noreply.github.com>
2025-05-22 18:17:07 +0200

    Add files via upload

---
+++ src/variant_interface.cpp
@@ -0,0 +1,61 @@
+#include "variant_interface.h"
+
+#include "common.h"
+#include "func_wrapper.h"
+#include "trace.h"
+#include "utility.h"
+
+VALIDATE_SIZE(variant_interface, 0x58);
+
+variant_interface::variant_interface(conglomerate *a2)
+{
+    this->m_vtbl = 0x008835A0;
+    this->field_4 = a2;
+    this->field_8 = true;
+}
+
+variant_info *variant_interface::get_random_variant()
+{
+    if constexpr (0)
+    {
+    }
+    else
+    {
+        return (variant_info*)CDECL_CALL(0x004CAD00, this);
+    }
+}
+
+void variant_interface::apply_variant(string_hash a2)
+{
+    TRACE("variant_interface::apply_variant");
+
+    if constexpr (0)
+    {}
+    else
+    {
+        void (__fastcall * func)(void*, void*, string_hash) = (decltype(func))0x004E0920;
+        func(this, nullptr,a2);
+    }
+}
+
+void variant_interface::apply_variant(variant_info *info)
+{
+    TRACE("variant_interface::apply_variant");
+
+    if constexpr (0)
+    {}
+    else
+    {
+        CDECL_CALL(0x004DB110, this, info);
+    }
+}
+
+void variant_interface_patch()
+{
+    {
+        void (variant_interface::* apply_variant)(string_hash) = &variant_interface::apply_variant;
+        auto address = func_address( apply_variant);
+        REDIRECT(0x0066F402, address);
+        REDIRECT(0x0066F372, address);
+    }
+}
---
+++ src/variant_interface.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "generic_interface.h"
+#include "mashable_vector.h"
+#include "string_hash.h"
+
+struct conglomerate;
+struct variant_info
+{
+    string_hash hash;
+    unsigned __int16 field_4;
+    unsigned __int16 field_6;
+    unsigned __int8* parts;
+    char* ifl_frames;
+};
+
+struct nglMeshFile;
+struct nglMesh;
+struct nglMorphSet;
+struct variant_speaker_id_set;
+
+struct variant_interface : generic_interface {
+    conglomerate *field_4;
+    bool field_8;
+    mashable_vector<variant_info> variants;
+    mashable_vector<variant_speaker_id_set> field_14;
+    variant_info *field_1C;
+    nglMesh *field_20;
+    nglMorphSet *field_24;
+    nglMeshFile *field_28;
+    int field_2C;
+    int field_30;
+    int field_34;
+    int field_38;
+    int field_3C;
+    int field_40;
+    int field_44;
+    int field_48;
+    int field_4C;
+    int field_50;
+    int field_54;
+
+    variant_interface(conglomerate *);
+
+    variant_info *get_random_variant();
+
+    void apply_variant(string_hash a2);
+
+    void apply_variant(variant_info *info);
+
+};
+
+extern void variant_interface_patch();
